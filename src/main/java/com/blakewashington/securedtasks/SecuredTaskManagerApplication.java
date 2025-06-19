f60eae224e7f510c2b2f96de334fa1dd88b61fbb
Blake Washington <blakewash1@gmail.com>
2025-06-19 12:46:53 -0400

    add initial files

---
+++ src/main/java/com/blakewashington/securedtasks/SecuredTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.blakewashington.securedtasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredTaskManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blakewashington/securedtasks/SecuredTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blakewashington.securedtasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
