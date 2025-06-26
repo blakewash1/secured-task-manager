6f964aee572bdc2df8010b644ada711b2e89b02a
Blake Washington <blakewash1@gmail.com>
2025-06-26 00:25:11 -0400

    create get, post, put, and delete requests in TaskController

--- src/main/java/com/blakewashington/securedtasks/controller/TaskController.java
+++ src/main/java/com/blakewashington/securedtasks/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.blakewashington.securedtasks.controller;
 
 import com.blakewashington.securedtasks.entity.Task;
 import com.blakewashington.securedtasks.repository.TaskRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,11 +27,26 @@ public class TaskController {
         return taskRepository.findAll();
     }
 
-//    @PutMapping("/{id}")
-//    public Task updateTask(@PathVariable int id, @RequestBody Task task) {
-//        return taskRepository.findById(id)
-//                .map(task -> {
-//                    task.
-//                })
-//    }
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
+        return taskRepository.findById(id)
+                .map(newTask -> {
+                    newTask.setTitle(task.getTitle());
+                    newTask.setDescription(task.getDescription());
+                    newTask.setPriority(task.getPriority());
+                    newTask.setCompleted(task.isCompleted());
+                    newTask.setCreatedAt(task.getCreatedAt());
+                    return ResponseEntity.ok(taskRepository.save(newTask));
+                })
+                .orElse(ResponseEntity.notFound().build()).getBody();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        if (!taskRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        taskRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
